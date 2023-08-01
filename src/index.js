461c8a641e031fcd5e30f87552d9f9b154e32a63
Anna Golosova <golosova76@gmail.com>
2023-08-01 16:09:57 +0300

    feat: solution the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function toReadable (number) {
+  let units = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  let tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
   
+  let resultWorld = '';
+
+  if (number === 0) {
+    resultWorld = units[0];
+  }
+
+  if (number > 99) {
+    resultWorld += units[Math.floor(number / 100)] + ' hundred';
+    number %= 100;
+    if (number) {
+      resultWorld += ' ';
+    }
+  }
+
+  if (number > 19) {
+    resultWorld += tens[Math.floor(number / 10)];
+    number %= 10;
+    if (number) {
+      resultWorld += ' ';
+    }
+  }
+
+  if (number) {
+    resultWorld += units[number];
+  }
+
+  return resultWorld;
 }
